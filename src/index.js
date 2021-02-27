cde24154006caafaf3967bb2c2fad3669d2d4eab
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-27 12:37:52 +0100

    initialize and add auth reducer for redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App';
+import reducers from './reducers';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
+// const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, {}, applyMiddleware());
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
 
